49c4e9b17e86295188e918d3bf75300cad908324
Emopandas <emopandas@emopandas-HP-Pavilion-dv2-Notebook-PC.(none)>
2012-05-30 19:26:38 -0500

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'aa39a547e6f66b5c6e60fce099f2743e385ae1209596281965584c9e6d773e1b69d5f323e436797f9087be0f062be3fba39bcf82a9b78cab07fccb492bad5b03'
